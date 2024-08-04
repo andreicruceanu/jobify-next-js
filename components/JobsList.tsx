88f8c9f0eeb7c07fffe4765560977cb37140aa01
Cruceanu Andrei <cruceanuandrei10@gmail.com>
2024-08-05 02:18:20 +0300

    Job List components

---
+++ components/JobsList.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { getAllJobsAction } from "@/utils/action";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import JobCard from "./JobCard";
+
+const JobsList = () => {
+  const searchParams = useSearchParams();
+
+  const search = searchParams.get("search") || "";
+  const jobStatus = searchParams.get("jobStatus") || "all";
+
+  const pageNumber = Number(searchParams.get("page")) || 1;
+
+  const { data, isPending } = useQuery({
+    queryKey: ["jobs", search, jobStatus, pageNumber],
+    queryFn: () => getAllJobsAction({ search, jobStatus, page: pageNumber }),
+  });
+
+  const jobs = data?.jobs || [];
+
+  if (isPending) return <h2 className="text-xl">Please await...</h2>;
+  if (jobs.length < 1) return <h2 className="text-xl">No jobs Found...</h2>;
+
+  return (
+    <>
+      <div className="grid md:grid-cols-2 gap-8">
+        {jobs.map((job) => {
+          return <JobCard key={job.id} job={job} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default JobsList;
